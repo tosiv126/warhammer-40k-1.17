49cc7d0269226b035ad4afc8f7fadeb7769bcb33
Joshua Schirra <j-schirra@gmx.de>
2021-08-16 14:56:42 +0200

    make fabrication bench rotatable

--- src/main/java/com/tosiv/warhammer/block/FabricationBenchBlock.java
+++ src/main/java/com/tosiv/warhammer/block/FabricationBenchBlock.java
@@ -3,7 +3,9 @@ package com.tosiv.warhammer.block;
 import com.tosiv.warhammer.screen.FabricationBenchScreenHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
@@ -13,15 +15,28 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class FabricationBenchBlock extends Block {
+public class FabricationBenchBlock extends HorizontalFacingBlock {
 
     public static final Property<Direction> FACING = HorizontalFacingBlock.FACING;
 
     public FabricationBenchBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
     }
 
     @Override
