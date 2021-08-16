6b5e849abef4382b08be5d2af5cc6b0d8cb9c5ed
Joshua Schirra <j-schirra@gmx.de>
2021-08-16 21:54:48 +0200

    refactor fabrication bench recipe format

--- src/main/java/com/tosiv/warhammer/crafting/FabricationBenchRecipe.java
+++ src/main/java/com/tosiv/warhammer/crafting/FabricationBenchRecipe.java
@@ -3,15 +3,20 @@ package com.tosiv.warhammer.crafting;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.tosiv.warhammer.util.registry.ModBlocks;
 import com.tosiv.warhammer.util.registry.ModRecipes;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.StringNbtReader;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.recipe.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
-import net.minecraft.util.Pair;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 
@@ -25,23 +30,25 @@ public class FabricationBenchRecipe implements Recipe<PlayerInventory> {
 
     private final Identifier id;
     private final ItemStack result;
-    private final DefaultedList<Pair<Ingredient, Integer>> input;
+    private final DefaultedList<ItemStack> inputs;
 
-    public FabricationBenchRecipe(Identifier id, ItemStack result, DefaultedList<Pair<Ingredient, Integer>> input) {
+    public FabricationBenchRecipe(Identifier id, ItemStack result, DefaultedList<ItemStack> inputs) {
         this.id = id;
         this.result = result;
-        this.input = input;
+        this.inputs = inputs;
     }
 
     @Override
     public boolean matches(PlayerInventory inventory, World world) {
-        return gatherIngredients(inventory, false).isEmpty();
+        return countMissingItems(inventory, false) == 0;
     }
 
-    private List<Pair<Ingredient, Integer>> gatherIngredients(PlayerInventory inventory, boolean consumeItems) {
+    private int countMissingItems(PlayerInventory inventory, boolean consumeItems) {
         // make a deep copy of the inputs that we can subsequently modify
-        List<Pair<Ingredient, Integer>> copy = new ArrayList<>();
-        input.forEach(pair -> copy.add(new Pair<>(pair.getLeft(), pair.getRight())));
+        List<ItemStack> missingItems = new ArrayList<>(inputs.size());
+        for (ItemStack input : inputs) {
+            missingItems.add(input.copy());
+        }
 
         inv:
         for (int i = 0; i < inventory.size(); i++) {
@@ -49,19 +56,16 @@ public class FabricationBenchRecipe implements Recipe<PlayerInventory> {
             if (!invStack.isEmpty()) {
                 ItemStack stack = invStack.copy();
                 int amount = stack.getCount();
-                for (Iterator<Pair<Ingredient, Integer>> iterator = copy.iterator(); iterator.hasNext(); ) {
-                    Pair<Ingredient, Integer> pair = iterator.next();
-                    if (pair.getLeft().test(stack)) {
-                        int remaining = pair.getRight();
-                        int toConsume = Math.min(amount, remaining);
-                        if(consumeItems) {
+                for (Iterator<ItemStack> iterator = missingItems.iterator(); iterator.hasNext(); ) {
+                    ItemStack toSearch = iterator.next();
+                    if (ItemStack.areItemsEqual(toSearch, stack)) {
+                        int toConsume = Math.min(amount, toSearch.getCount());
+                        if (consumeItems) {
                             inventory.removeStack(i, toConsume);
                         }
-                        remaining -= toConsume;
-                        if (remaining <= 0) {
+                        toSearch.decrement(toConsume);
+                        if (toSearch.isEmpty()) {
                             iterator.remove();
-                        } else {
-                            pair.setRight(remaining);
                         }
                         if (stack.isEmpty()) {
                             continue inv;
@@ -70,12 +74,12 @@ public class FabricationBenchRecipe implements Recipe<PlayerInventory> {
                 }
             }
         }
-        return copy;
+        return missingItems.stream().filter(stack -> !stack.isEmpty()).mapToInt(ItemStack::getCount).sum();
     }
 
     @Override
     public ItemStack craft(PlayerInventory inventory) {
-        gatherIngredients(inventory, true);
+        countMissingItems(inventory, true);
         return getOutput().copy();
     }
 
@@ -87,6 +91,11 @@ public class FabricationBenchRecipe implements Recipe<PlayerInventory> {
     @Override
     public ItemStack getOutput() {
         return result;
+    }
+
+    @Override
+    public ItemStack createIcon() {
+        return new ItemStack(ModBlocks.IMPERIAL_FABRICATION_BENCH);
     }
 
     @Override
@@ -104,57 +113,79 @@ public class FabricationBenchRecipe implements Recipe<PlayerInventory> {
         return ModRecipes.FABRICATION_BENCH;
     }
 
+    public DefaultedList<ItemStack> getInputs() {
+        return inputs;
+    }
+
     @Override
-    public ItemStack createIcon() {
-        return new ItemStack(ModBlocks.IMPERIAL_FABRICATION_BENCH);
+    public DefaultedList<Ingredient> getIngredients() {
+        DefaultedList<Ingredient> value = DefaultedList.ofSize(inputs.size(), Ingredient.EMPTY);
+        for (int i = 0; i < inputs.size(); i++) {
+            value.set(i, Ingredient.ofStacks(inputs.get(i)));
+        }
+        return value;
     }
 
     public static class Serializer implements RecipeSerializer<FabricationBenchRecipe> {
 
+        private static ItemStack getStack(JsonObject json) {
+            Item item = ShapedRecipe.getItem(json);
+            int count = JsonHelper.getInt(json, "count", 1);
+            if (count < 1) {
+                throw new JsonSyntaxException("Invalid output count: " + count);
+            }
+            ItemStack value = new ItemStack(item, count);
+            if (json.has("data")) {
+                try {
+                    String snbt = JsonHelper.getString(json, "data");
+                    NbtCompound nbt = new StringNbtReader(new StringReader(snbt)).parseCompound();
+                    value.setTag(nbt);
+                } catch (CommandSyntaxException e) {
+                    throw new JsonSyntaxException("unable to parse data tag", e);
+                }
+            }
+            return value;
+        }
+
         @Override
         public FabricationBenchRecipe read(Identifier id, JsonObject json) {
             JsonArray ingredients = JsonHelper.getArray(json, "ingredients");
-            DefaultedList<Pair<Ingredient, Integer>> input = DefaultedList.of();
+            DefaultedList<ItemStack> inputs = DefaultedList.of();
 
             for (int i = 0; i < ingredients.size(); i++) {
                 JsonObject element = JsonHelper.asObject(ingredients.get(i), "ingredients[" + i + "]");
-                Ingredient ingredient = Ingredient.fromJson(element);
-                int amount = JsonHelper.getInt(element, "count", 1);
-                if (!ingredient.isEmpty() && amount > 0) {
-                    input.add(new Pair<>(ingredient, amount));
+                ItemStack stack = getStack(element);
+                if(!stack.isEmpty()) {
+                    inputs.add(stack);
                 }
             }
-            if (input.isEmpty()) {
+            if (inputs.isEmpty()) {
                 throw new JsonParseException("No ingredients for recipe");
-            } else if (input.size() > MAX_ALLOWED_ITEMS) {
+            } else if (inputs.size() > MAX_ALLOWED_ITEMS) {
                 throw new JsonParseException("Too many ingredients for recipe");
             } else {
                 ItemStack result = ShapedRecipe.outputFromJson(JsonHelper.getObject(json, "result"));
-                return new FabricationBenchRecipe(id, result, input);
+                return new FabricationBenchRecipe(id, result, inputs);
             }
         }
 
         @Override
         public FabricationBenchRecipe read(Identifier id, PacketByteBuf buf) {
             int size = buf.readVarInt();
-            DefaultedList<Pair<Ingredient, Integer>> input = DefaultedList.ofSize(size, new Pair<>(Ingredient.EMPTY, 0));
-
-            for (int j = 0; j < input.size(); ++j) {
-                Ingredient ingredient = Ingredient.fromPacket(buf);
-                int amount = buf.readVarInt();
-                input.set(j, new Pair<>(ingredient, amount));
+            DefaultedList<ItemStack> inputs = DefaultedList.ofSize(size, ItemStack.EMPTY);
+            for (int i = 0; i < inputs.size(); i++) {
+                ItemStack stack = buf.readItemStack();
+                inputs.set(i, stack);
             }
-
             ItemStack result = buf.readItemStack();
-            return new FabricationBenchRecipe(id, result, input);
+            return new FabricationBenchRecipe(id, result, inputs);
         }
 
         @Override
         public void write(PacketByteBuf buf, FabricationBenchRecipe recipe) {
-            buf.writeVarInt(recipe.input.size());
-            for (Pair<Ingredient, Integer> entry : recipe.input) {
-                entry.getLeft().write(buf);
-                buf.writeVarInt(entry.getRight());
+            buf.writeVarInt(recipe.getInputs().size());
+            for (ItemStack stack : recipe.getInputs()) {
+                buf.writeItemStack(stack);
             }
             buf.writeItemStack(recipe.result);
         }
